851912417f9be6082a6737b81ef87bae794aeda5
jodavaga <jodavaga@gmail.com>
2020-02-06 12:50:00 -0500

    RegForm - formBuilder implemented and validations added. Erors handle

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,5 @@
+export class UsuarioModel {
+    email: string;
+    name: string;
+    password: string;
+}
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  registroForm: FormGroup;
+  usuario: UsuarioModel;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
 
-  ngOnInit() { }
+   }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+
+    // Initialize form
+    this.registroForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+  }
+  
+  onSubmit() {
+    console.warn('Formulario enviado!');
+    // Fills user info with form values
+    this.usuario = this.registroForm.value;
+    console.log( this.usuario );
+   
+  }
 
 
 }
